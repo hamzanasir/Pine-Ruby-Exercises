d7cb182cfc68bdc1b9b7d1034df65b8d0f06fff2
Hamza Nasir <mnasir@hawk.iit.edu>
2018-01-20 16:44:17 -0600

    Finsished chapter 6.2 table of contents exercise

---
+++ chapter6/6.2.2.rb
@@ -0,0 +1,5 @@
+line_width = 50
+puts 'Table of contents'.center(line_width)
+puts 'Chapter 1:'.ljust(line_width/3) + 'Getting Started'.center(line_width/3) + 'Page 1'.rjust(line_width/3)
+puts 'Chapter 2:'.ljust(line_width/3) + 'Numbers'.center(line_width/3) + 'Page 9'.rjust(line_width/3)
+puts 'Chapter 3:'.ljust(line_width/3) + 'Letters'.center(line_width/3) + 'Page 13'.rjust(line_width/3)
