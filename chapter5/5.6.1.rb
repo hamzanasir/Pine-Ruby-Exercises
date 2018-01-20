4d2ce114db5d38849e6b57e8fcfb86f37b596cb7
Hamza Nasir <mnasir@hawk.iit.edu>
2018-01-20 16:17:17 -0600

    Did the full name greeting exercise in chapter 5 of the pine book

---
+++ chapter5/5.6.1.rb
@@ -0,0 +1,7 @@
+puts "Hi there! What's your first name?"
+fname = gets.chomp
+puts "Alrighty there #{fname}, What is your middle name?"
+mname = gets.chomp
+puts "Great! And your last name?"
+lname = gets.chomp
+puts "Thank you. Welcome #{fname} #{mname} #{lname}"
